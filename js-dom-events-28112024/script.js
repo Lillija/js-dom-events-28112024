63a8a3b61c086e5f1e7bbdc0bb20c7c38ca0e926
Lillija <asikeligalillija@gmail.com>
2024-11-28 14:01:46 +0200

    Add files via upload

---
+++ js-dom-events-28112024/script.js
@@ -0,0 +1,12 @@
+const body = document.body
+const div = document.querySelector('div')
+const spanBye = document.querySelector('#ToBeRemoved')
+
+body.append("Added with js")
+
+console.log(text.dataset)
+
+spanBye.remove()
+
+Title.style.color = "red"
+Title.style.backgroundColor = "black"
